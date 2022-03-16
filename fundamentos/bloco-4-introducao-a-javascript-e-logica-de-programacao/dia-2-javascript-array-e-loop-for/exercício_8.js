469c36033c79b81c633728c66392dcd706ba72cd
Mellynna <mell.evelyn@hotmail.com>
2022-03-16 08:09:11 -0300

    exercicios bloco4/dia2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercício_8.js
@@ -0,0 +1,6 @@
+// 8. Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+let numbersAte25 = [];
+for (let i = 1; i <= 25; i += 1) {
+    numbersAte25.push(i);
+}
+console.log('Valores de 1 até 25: ', numbersAte25);
